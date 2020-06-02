5e2bb20665ce5d37e9133ea4b42a2434804ea879
Felipe Berrios <felipeberrios@MacBook-Pro-de-Felipe.local>
2020-06-02 16:27:40 -0500

    Updates

---
+++ precache-manifest.93256f73ce5c9ba91ecb28b01242630d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce82f69af18c4a6b6f06be5f65b48126",
+    "url": "/test-react-1/index.html"
+  },
+  {
+    "revision": "c1aa8c0174599f006ea6",
+    "url": "/test-react-1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a45a7e60c64cd90fcce8",
+    "url": "/test-react-1/static/js/2.2de68096.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/test-react-1/static/js/2.2de68096.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c1aa8c0174599f006ea6",
+    "url": "/test-react-1/static/js/main.d68d8658.chunk.js"
+  },
+  {
+    "revision": "46b333f57efbfb786c37",
+    "url": "/test-react-1/static/js/runtime-main.d2695a7e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/test-react-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
